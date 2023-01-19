2aeda3d58bda77396cb244458d33657e323be264
Daniil Volynkin <dvvolynkin@avito.ru>
2023-01-19 17:21:57 +0300

    Add stubs server

--- gateway/generate_server_confs.py
+++ gateway/generate_server_confs.py
@@ -5,16 +5,17 @@ RESTY_CONF_DIR = os.environ["RESTY_CONF_DIR"]
 NGINX_CONF_DIR = os.environ["NGINX_CONF_DIR"]
 GATEWAY_SERVERS_CONFIG_FILE = os.environ["GATEWAY_SERVERS_CONFIG_FILE"]
 MAIN_DOMAIN = os.environ["MAIN_DOMAIN"]
+NGINX_STUBS_SERVER_NAME = os.environ.get('NGINX_STUBS_SERVER_NAME', 'gateway')
 
 if __name__ == '__main__':
     # Читаем шаблон server-proxy.conf
     with open('./gateway.json') as f:
         gateway_conf = json.load(f)
-    with open(f"{RESTY_CONF_DIR}/server-proxy.conf") as template_file:
+    with open(f"{RESTY_CONF_DIR}/server-proxy.conf", "r") as template_file:
         server_proxy_template = template_file.read()
 
     # Читаем шаблон server-proxy-location.conf
-    with open(f"{RESTY_CONF_DIR}/server-proxy-location.conf") as template_file:
+    with open(f"{RESTY_CONF_DIR}/server-proxy-location.conf", "r") as template_file:
         server_proxy_location_template = template_file.read()
 
     # Для каждого сайта из списка SITES
@@ -41,3 +42,10 @@ if __name__ == '__main__':
         # Записываем результат в файл ${NGINX_CONF_DIR}/{server_name}.conf
         with open(f"{NGINX_CONF_DIR}/{server_name}.conf", "w") as out_file:
             out_file.write(server_proxy_str)
+
+    with open(f'{RESTY_CONF_DIR}/stubs-server.conf', 'r') as template_file:
+        template = template_file.read()
+        result_config = template.replace('$STUBS_SERVER_NAME', NGINX_STUBS_SERVER_NAME)
+        with open(f"{NGINX_CONF_DIR}/{NGINX_STUBS_SERVER_NAME}.conf", "w") as out_file:
+            out_file.write(result_config)
+
